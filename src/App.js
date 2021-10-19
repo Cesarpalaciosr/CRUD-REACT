fabd4956a1c74d87a43dc5ffa5b9d1d349251dd2
CesarP <capr99@hotmail.com>
2021-10-18 22:45:45 -0400

    primer push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+class FlavorForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+       names:'Cesar',
+       last_name:'Palacios',
+        id: '2312312',
+        gender:'hombre',
+      };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = value.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    console.log()
+    event.preventDefault();
+  }
+
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input name="names"
+              type="text" value={this.state.value} onChange={this.handleInputChange}/>
+          </label>
+          <label>
+            Last name:
+            <input name = "last_name"
+              type="text"value={this.state.value} onChange={this.handleInputChange}/>
+          </label>
+          <label>
+            identification:
+            <input name="id"
+            type="number" value={this.state.value} onChange={this.handleInputChange} />
+            <label>
+            Sexo:
+            <select name= "gender" value={this.state.value} onChange={this.handleInputChange} >
+              <option value="Hombre">Hombre</option>
+              <option value="Mujer">Mujer</option>
+            </select>
+          </label>
+          <input type="submit" value="Submit" />
+          </label>
+        </form>
+      );
+    }
+  }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>This is my component: <FlavorForm></FlavorForm></div>
+      
   );
 }
 
---
+++ src/utils/pool.js
@@ -0,0 +1,24 @@
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'CRUD-REACT',
+    password: 'masterkey',
+    port: 5432,
+})
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end()
+})
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'CRUD-REACT',
+    password: 'masterkey',
+    port: 5432,
+})
+client.connect()
+client.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    client.end()
+})
